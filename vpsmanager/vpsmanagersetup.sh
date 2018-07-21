88a2fbc7473ae0c1a2baab1e93a4fdfc4d5a6f69
cantalupo555 <cantalupo@protonmail.com>
2018-07-21 05:56:30 -0300

    Update

--- vpsmanager/vpsmanagersetup.sh
+++ vpsmanager/vpsmanagersetup.sh
@@ -128,7 +128,7 @@ then
 	echo "acl url3 dstdomain -i $ipdovps" > /tmp/sqd2
 	wget https://raw.githubusercontent.com/cantalupo555/phreaker56/master/vpsmanager/squid.txt -O /tmp/sqd3
 	cat /tmp/sqd1 /tmp/sqd2 /tmp/sqd3 > /etc/squid/squid.conf
-	wget http://phreaker56.obex.pw/vpsmanager/payload.txt -O /etc/squid/payload.txt
+	wget https://raw.githubusercontent.com/cantalupo555/phreaker56/master/vpsmanager/payload.txt -O /etc/squid/payload.txt
 	echo " " >> /etc/squid/payload.txt
 	grep -v "^Port 443" /etc/ssh/sshd_config > /tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
 	echo "Port 443" >> /etc/ssh/sshd_config
